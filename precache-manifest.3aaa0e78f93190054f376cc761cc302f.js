1e0cc44669887e5ae20dabb2477cb82321cc0738
VigneshVasanth <149473107+Vignesh81004@users.noreply.github.com>
2025-05-08 08:41:13 +0530

    Add files via upload

---
+++ precache-manifest.3aaa0e78f93190054f376cc761cc302f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2db33baeb2a79ba9da99a5ced30958",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a7c1d0fa36a09ff9d8a",
+    "url": "/static/css/main.f5a08245.chunk.css"
+  },
+  {
+    "revision": "2094ebc825bc8fc1b50f",
+    "url": "/static/js/2.71e61231.chunk.js"
+  },
+  {
+    "revision": "fe377a2c5856901f0df746b3fdaf5666",
+    "url": "/static/js/2.71e61231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7c1d0fa36a09ff9d8a",
+    "url": "/static/js/main.6221d387.chunk.js"
+  },
+  {
+    "revision": "5d72459d2a0769b65f30",
+    "url": "/static/js/runtime-main.f381c30c.js"
+  },
+  {
+    "revision": "ba78eef3a8e48d5ee3fcf86ce0488b5d",
+    "url": "/static/media/banner.ba78eef3.png"
+  },
+  {
+    "revision": "3b60236561f786e9096c0564eb57dbd7",
+    "url": "/static/media/gym.3b602365.png"
+  }
+]);
